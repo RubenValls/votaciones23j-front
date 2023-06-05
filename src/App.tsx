b557ef684836a7f400c0225e37c4af2537f9651f
Rubén Valls <r.vallsaparici@gmail.com>
2023-06-05 17:51:49 +0200

    added MaterialUI to Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Button from '@mui/material/Button';
 
 function App() {
 
   return (
     <>
-      
+      <Button variant="contained">Hello World</Button>
     </>
   )
 }
