25be95b00b32dc8146d19acf8366b22c1be2eb89
Rubén Valls <r.vallsaparici@gmail.com>
2023-06-05 18:18:48 +0200

    added material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import './App.css'
-import Button from '@mui/material/Button';
+import { Box, Grid } from "@mui/material";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <Button variant="contained">Hello World</Button>
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
+          <Box sx={{ bgcolor: 'red', height: 'auto', width: '100%' }}>xs=8</Box>
+        </Grid>
+        <Grid item xs={10}>
+          <Box sx={{ bgcolor: 'red', height: 'auto', width: '100%' }}>xs=4</Box>
+        </Grid>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
