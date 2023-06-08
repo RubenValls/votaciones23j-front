f3ac93e090f820a4d91be4d8307a17f4e56ab256
Rubén Valls <r.vallsaparici@gmail.com>
2023-06-08 12:08:37 +0200

    added partidos logo to the main page but not redirecting to details page yet.

--- src/pages/inicio/components/LogosPartidos.tsx
+++ src/pages/inicio/components/LogosPartidos.tsx
@@ -1,13 +1,20 @@
-import { Image, Box } from '@chakra-ui/react'
+import { Image, Wrap, WrapItem } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom';
 
 export default function LogosPartidos({partidos}: any) {
+    const navigate = useNavigate();
+    
   return (
-        partidos.map((partido: any) => {
-            return(
-                <Box boxSize='sm' key={partido?.id}>
-                    <Image src={partido?.attributes?.logo} alt={partido?.attributes?.nombre}/>
-                </Box>
-            )
-        })
+        <Wrap spacing="100px" justify="center" p={10}>
+            {
+                partidos.map((partido: any) => {
+                    return(
+                        <WrapItem maxWidth="200px" key={partido?.id} onClick={() => navigate("/votos")}>
+                            <Image src={partido?.attributes?.logo} alt={partido?.attributes?.nombre}/>
+                        </WrapItem>
+                    )
+                })
+            }
+        </Wrap>
   )
 }
